8ec1eda3996f598bcdd4e057cc4b66bb42a66fc1
zzpu <dotzy>
2021-03-10 11:12:57 +0800

    feat:pkg

--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.15
 require (
 	github.com/LyricTian/structs v1.1.1
 	github.com/bwmarrin/snowflake v0.3.0
+	github.com/go-kratos/kratos/cmd/protoc-gen-go-errors v0.0.0-20210217095515-c4e4aa563867 // indirect
+	github.com/go-kratos/kratos/v2 v2.0.0-20210217083752-d86d233d93ce
 	github.com/google/uuid v1.2.0
 	github.com/json-iterator/go v1.1.10
+	github.com/opentracing/opentracing-go v1.2.0
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.4.6
+	google.golang.org/grpc v1.35.0
 	gopkg.in/yaml.v2 v2.4.0
 )
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"math/rand"
 	"reflect"
+	"time"
 
 	"github.com/LyricTian/structs"
 )
@@ -30,4 +32,16 @@ func StructMapToStruct(s, ts interface{}) error {
 	}
 
 	return nil
+}
+
+func GetRandomString(length int) string {
+
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	var result []byte
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
